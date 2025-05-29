ba88f81d4e64d75737406fe3cae24107ebdc70b6
Dr3iundZwanzig <zaude.sven@gmail.com>
2025-05-29 13:35:29 +0000

    framerate limit added

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,11 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+    
+    
 
 
 
